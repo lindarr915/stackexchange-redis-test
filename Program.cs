e0a529a16d2e2f23cfb3f50ae8fc97dbc4fffc59
Darren Lin <lindarr@amazon.com>
2021-09-17 09:00:53 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using StackExchange.Redis;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+
+namespace Redistest
+{
+    class Program
+    {
+
+        static void WriteDataToRedis()
+        {
+            Lazy<ConnectionMultiplexer> lazyConnection = CreateConnection();
+
+            IDatabase cache = GetDatabase();
+
+            for (int i = 0; ; i++)
+            {
+                cache.StringSet(Guid.NewGuid().ToString(), LoremIpsum(20, 40, 2, 3, 4));
+                if (i % 1000 == 0) Console.WriteLine(i.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            InitializeConfiguration();
+            IDatabase cache = GetDatabase();
+
+            // Test t = new Test();  
+            Thread[] tr = new Thread[200];  
+            for (int i = 0; i < 200; i++)  
+            {  
+                tr[i] = new Thread(new ThreadStart(WriteDataToRedis));  
+                tr[i].Name = String.Format("Working Thread: {0}", i);  
+            }  
+            //Start each thread  
+            foreach (Thread x in tr)  
+            {  
+                x.Start();  
+            }  
+            Console.ReadKey();  
+
+            Console.WriteLine("In Main: Creating the Child thread");
+            // newThread.Join();
+
+            // Perform cache operations using the cache object...
+
+            // Simple PING command
+            string cacheCommand = "PING";
+            Console.WriteLine("\nCache command  : " + cacheCommand);
+            Console.WriteLine("Cache response : " + cache.Execute(cacheCommand).ToString());
+
+            // Simple get and put of integral data types into the cache
+            cacheCommand = "GET Message";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringGet()");
+            Console.WriteLine("Cache response : " + cache.StringGet("Message").ToString());
+
+            for (int i = 0; i < 2; i++)
+            {
+                cache.StringGet("Message", CommandFlags.PreferReplica);
+                Console.WriteLine("Reading Message from Redis...");
+                
+            }
+
+            cacheCommand = "SET Message \"Hello! The cache is working from a .NET Core console app!\"";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringSet()");
+            Console.WriteLine("Cache response : " + cache.StringSet("Message", "Hello! The cache is working from a .NET Core console app!").ToString());
+
+            // Demonstrate "SET Message" executed as expected...
+            cacheCommand = "GET Message";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringGet()");
+            Console.WriteLine("Cache response : " + cache.StringGet("Message").ToString());
+
+            CloseConnection(lazyConnection);
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection = CreateConnection();
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+
+        private static string cacheConnection = "darren-demo.lm5w0w.clustercfg.usw2.cache.amazonaws.com:6379";
+
+        private static Lazy<ConnectionMultiplexer> CreateConnection()
+        {
+            return new Lazy<ConnectionMultiplexer>(() =>
+            {
+                return ConnectionMultiplexer.Connect(cacheConnection);
+            });
+        }
+
+        private static long lastReconnectTicks = DateTimeOffset.MinValue.UtcTicks;
+        private static DateTimeOffset firstErrorTime = DateTimeOffset.MinValue;
+        private static DateTimeOffset previousErrorTime = DateTimeOffset.MinValue;
+
+        private static readonly object reconnectLock = new object();
+
+        // In general, let StackExchange.Redis handle most reconnects,
+        // so limit the frequency of how often ForceReconnect() will
+        // actually reconnect.
+        public static TimeSpan ReconnectMinFrequency => TimeSpan.FromSeconds(60);
+
+        // If errors continue for longer than the below threshold, then the
+        // multiplexer seems to not be reconnecting, so ForceReconnect() will
+        // re-create the multiplexer.
+        public static TimeSpan ReconnectErrorThreshold => TimeSpan.FromSeconds(30);
+
+        public static int RetryMaxAttempts => 5;
+
+        private static void CloseConnection(Lazy<ConnectionMultiplexer> oldConnection)
+        {
+            if (oldConnection == null)
+                return;
+
+            try
+            {
+                oldConnection.Value.Close();
+            }
+            catch (Exception)
+            {
+                // Example error condition: if accessing oldConnection.Value causes a connection attempt and that fails.
+            }
+        }
+
+        /// <summary>
+        /// Force a new ConnectionMultiplexer to be created.
+        /// NOTES:
+        ///     1. Users of the ConnectionMultiplexer MUST handle ObjectDisposedExceptions, which can now happen as a result of calling ForceReconnect().
+        ///     2. Don't call ForceReconnect for Timeouts, just for RedisConnectionExceptions or SocketExceptions.
+        ///     3. Call this method every time you see a connection exception. The code will:
+        ///         a. wait to reconnect for at least the "ReconnectErrorThreshold" time of repeated errors before actually reconnecting
+        ///         b. not reconnect more frequently than configured in "ReconnectMinFrequency"
+        /// </summary>
+        public static void ForceReconnect()
+        {
+            var utcNow = DateTimeOffset.UtcNow;
+            long previousTicks = Interlocked.Read(ref lastReconnectTicks);
+            var previousReconnectTime = new DateTimeOffset(previousTicks, TimeSpan.Zero);
+            TimeSpan elapsedSinceLastReconnect = utcNow - previousReconnectTime;
+
+            // If multiple threads call ForceReconnect at the same time, we only want to honor one of them.
+            if (elapsedSinceLastReconnect < ReconnectMinFrequency)
+                return;
+
+            lock (reconnectLock)
+            {
+                utcNow = DateTimeOffset.UtcNow;
+                elapsedSinceLastReconnect = utcNow - previousReconnectTime;
+
+                if (firstErrorTime == DateTimeOffset.MinValue)
+                {
+                    // We haven't seen an error since last reconnect, so set initial values.
+                    firstErrorTime = utcNow;
+                    previousErrorTime = utcNow;
+                    return;
+                }
+
+                if (elapsedSinceLastReconnect < ReconnectMinFrequency)
+                    return; // Some other thread made it through the check and the lock, so nothing to do.
+
+                TimeSpan elapsedSinceFirstError = utcNow - firstErrorTime;
+                TimeSpan elapsedSinceMostRecentError = utcNow - previousErrorTime;
+
+                bool shouldReconnect =
+                    elapsedSinceFirstError >= ReconnectErrorThreshold // Make sure we gave the multiplexer enough time to reconnect on its own if it could.
+                    && elapsedSinceMostRecentError <= ReconnectErrorThreshold; // Make sure we aren't working on stale data (e.g. if there was a gap in errors, don't reconnect yet).
+
+                // Update the previousErrorTime timestamp to be now (e.g. this reconnect request).
+                previousErrorTime = utcNow;
+
+                if (!shouldReconnect)
+                    return;
+
+                firstErrorTime = DateTimeOffset.MinValue;
+                previousErrorTime = DateTimeOffset.MinValue;
+
+                Lazy<ConnectionMultiplexer> oldConnection = lazyConnection;
+                CloseConnection(oldConnection);
+                lazyConnection = CreateConnection();
+                Interlocked.Exchange(ref lastReconnectTicks, utcNow.UtcTicks);
+            }
+        }
+
+        // In real applications, consider using a framework such as
+        // Polly to make it easier to customize the retry approach.
+        private static T BasicRetry<T>(Func<T> func)
+        {
+            int reconnectRetry = 0;
+            int disposedRetry = 0;
+
+            while (true)
+            {
+                try
+                {
+                    return func();
+                }
+                catch (Exception ex) when (ex is RedisConnectionException || ex is SocketException)
+                {
+                    reconnectRetry++;
+                    if (reconnectRetry > RetryMaxAttempts)
+                        throw;
+                    ForceReconnect();
+                }
+                catch (ObjectDisposedException)
+                {
+                    disposedRetry++;
+                    if (disposedRetry > RetryMaxAttempts)
+                        throw;
+                }
+            }
+        }
+
+        public static IDatabase GetDatabase()
+        {
+            return BasicRetry(() => Connection.GetDatabase());
+        }
+
+        public static System.Net.EndPoint[] GetEndPoints()
+        {
+            return BasicRetry(() => Connection.GetEndPoints());
+        }
+
+        public static IServer GetServer(string host, int port)
+        {
+            return BasicRetry(() => Connection.GetServer(host, port));
+        }
+
+        private static IConfigurationRoot Configuration { get; set; }
+        const string SecretName = "CacheConnection";
+
+        private static void InitializeConfiguration()
+        {
+            var builder = new ConfigurationBuilder();
+
+            Configuration = builder.Build();
+        }
+
+        static string LoremIpsum(int minWords, int maxWords,
+            int minSentences, int maxSentences,
+            int numParagraphs)
+        {
+
+            var words = new[]{"lorem", "ipsum", "dolor", "sit", "amet", "consectetuer",
+        "adipiscing", "elit", "sed", "diam", "nonummy", "nibh", "euismod",
+        "tincidunt", "ut", "laoreet", "dolore", "magna", "aliquam", "erat"};
+
+            var rand = new Random();
+            int numSentences = rand.Next(maxSentences - minSentences)
+                + minSentences + 1;
+            int numWords = rand.Next(maxWords - minWords) + minWords + 1;
+
+            StringBuilder result = new StringBuilder();
+
+            for (int p = 0; p < numParagraphs; p++)
+            {
+                result.Append("<p>");
+                for (int s = 0; s < numSentences; s++)
+                {
+                    for (int w = 0; w < numWords; w++)
+                    {
+                        if (w > 0) { result.Append(" "); }
+                        result.Append(words[rand.Next(words.Length)]);
+                    }
+                    result.Append(". ");
+                }
+                result.Append("</p>");
+            }
+
+            return result.ToString();
+        }
+
+    }
+
+}
